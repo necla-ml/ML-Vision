211a8166ce82a1d76c2c988120ebe9c6dff35db3
Farley Lai <farleylai@nec-labs.com>
2021-07-27 00:24:58 -0700

    Support cv2 image as input to resize

--- ml/vision/transforms/functional.py
+++ ml/vision/transforms/functional.py
@@ -26,10 +26,10 @@ def is_tensor(img):
 
 def is_cv2(img):
     if isinstance(img, np.ndarray):
-        if len(img.shape) == 3:
+        if img.ndim == 3:
             return img.shape[-1] in (1, 3) and img.dtype == np.uint8
         else:
-            return len(img.shape) == 2 and img.dtype == np.uint8
+            return img.ndim == 2 and img.dtype == np.uint8
 
 def is_accimage(img):
     return accimage is not None and isinstance(img, accimage.Image)
@@ -41,7 +41,7 @@ def is_pil(img):
     else:
         return isinstance(img, Image.Image)
 
-def resize(img, size, constraint='shorter', interpolation=InterpolationMode.BILINEAR, **kwargs):
+def resize(img, size, interpolation=InterpolationMode.BILINEAR, constraint='shorter', **kwargs):
     '''Resize input image of PIL/accimage, OpenCV BGR or torch tensor.
     Args:
         size(Tuple[int], int): tuple of height and width or length on both sides following torchvision resize semantics
@@ -51,7 +51,7 @@ def resize(img, size, constraint='shorter', interpolation=InterpolationMode.BILI
     If the image is torch Tensor, it is expected
     to have [..., H, W] shape, where ... means an arbitrary number of leading dimensions
     Args:
-        img (PIL Image or Tensor): Image to be resized.
+        img (PIL Image or Tensor or cv2 numpy): Image to be resized.
         size (sequence or int): Desired output size. If size is a sequence like
             (h, w), the output size will be matched to this. If size is an int,
             the smaller edge of the image will be matched to this number maintaining
@@ -69,6 +69,8 @@ def resize(img, size, constraint='shorter', interpolation=InterpolationMode.BILI
     """
     if is_tensor(img):
         H, W = img.shape[-2:]
+    elif is_cv2(img):
+        H, W = img.shape[:2]
     else:
         # PIL
         W, H = img.size
@@ -80,7 +82,24 @@ def resize(img, size, constraint='shorter', interpolation=InterpolationMode.BILI
             else:
                 h, w = size, int(W / H * size)
         else:
-            return TF.resize(img, size, interpolation, **kwargs)
+            if H < W:
+                w, h = int(W / H * size), size
+            else:
+                w, h = size, int(H / W * size)
+            # return TF.resize(img, size, interpolation, **kwargs)
     else:
         h, w = size
-    return TF.resize(img, (h, w), interpolation, **kwargs)
+    
+    from ml import logging
+    if is_cv2(img):
+        from ml.av.backend import opencv as cv
+        logging.info(f"cv img shape={tuple(img.shape)}, dtype={img.dtype}, sum={img.sum(axis=(0,1))}")
+        return cv.resize(img, (w, h), interpolation=cv.INTER_LINEAR)
+    else:
+        if is_tensor(img):
+            logging.info(f"tensor img shape={tuple(img.shape)}, dtype={img.dtype}, sum={img.sum(dim=(1,2))}")
+        else:
+            import numpy as np
+            img_np = np.array(img)
+            logging.info(f"PIL img shape={tuple(img_np.shape)}, dtype={img_np.dtype}, sum={img_np.sum(axis=(0,1))}")
+        return TF.resize(img, (h, w), interpolation, **kwargs)
